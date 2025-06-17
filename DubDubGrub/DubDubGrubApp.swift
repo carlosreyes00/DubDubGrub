69d6ca07a758347f5511b2cb0f646fd9327704bf
Carlos Reyes <crrmagadan@icloud.com>
2025-06-17 18:45:12 -0400

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Carlos Reyes on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
