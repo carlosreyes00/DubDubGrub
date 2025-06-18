2e0d02b29dc4e34a2a259611b1016a965d41caee
Carlos Reyes <crrmagadan@icloud.com>
2025-06-18 19:56:03 -0400

    MapView - Show the map

--- DubDubGrub/LocationMapView.swift
+++ DubDubGrub/LocationMapView.swift
@@ -6,10 +6,28 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    @State private var cameraPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
+                                                                                                                   longitude: -121.891054),
+                                                                                    span: MKCoordinateSpan(latitudeDelta: 0.01,
+                                                                                                           longitudeDelta: 0.01)))
+    
     var body: some View {
-        Text("Map View")
+        ZStack {
+            Map(position: $cameraPosition)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image(.ddgMapLogo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .shadow(radius: 10)
+                Spacer()
+            }
+        }
     }
 }
 
