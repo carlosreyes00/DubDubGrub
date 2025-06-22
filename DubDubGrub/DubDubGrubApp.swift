7b46ecc7f59abf5a0f95af496fba561730b22caf
Carlos Reyes <crrmagadan@icloud.com>
2025-06-22 12:46:42 -0400

    DubDubGrubApp file done again

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Carlos Reyes on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppTabView()
+        }
+    }
+}
---
+++ DubDubGrub/Views/Screens/DubDubGrubApp.swift
@@ -0,0 +1,8 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Carlos Reyes on 6/22/25.
+//
+
+import Foundation
